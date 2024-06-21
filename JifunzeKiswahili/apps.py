febb0db4e010acb89ed047d51531d11307e805b2
Felix Rich Michael <159950700+Frich12@users.noreply.github.com>
2024-06-21 22:51:45 +0300

    added my django app

--- JifunzeKiswahili/apps.py
+++ JifunzeKiswahili/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SwamyConfig(AppConfig):
+class JifunzeKiswahiliConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'swamy'
+    name = 'JifunzeKiswahili'
